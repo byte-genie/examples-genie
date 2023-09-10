36b641a6d924373dd2c4b23924a4484619b55b7c
mh@imac <mjdhasan@icloud.com>
2023-09-10 19:36:17 +0800

    added document_procesing/long_pdf_processing.py

---
+++ document_processing/long_pdf_processing.py
@@ -0,0 +1,8 @@
+# # Long-form PDF document processing
+
+"""
+For long-form PDF document processing, see company_research folder, which contains:
+* document_sourcing.py (.ipynb): example of downloading PDF documents from the web;
+* document_processing.py (.ipynb): example of triggering text and quants extraction from PDF documents;
+* data_extraction.py (.ipynb): example of extracted data related to specific keyphrases/metrics from documents, and re-structuring that data into a desired form.
+"""
