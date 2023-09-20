f9bd4e52ecc52994ce4e81e592dcf124c479f07b
mh@imac <mjdhasan@icloud.com>
2023-09-20 16:21:35 +0800

    read uploaded files

--- document_processing/kpi_extraction.py
+++ document_processing/kpi_extraction.py
@@ -53,19 +53,11 @@ logger.info(
     f"Time taken to upload {len(upload_responses)} documents: "
     f"{(end_time - start_time) / 60} min"
 )
-# ### Check uploaded documents
-# missing_files = []
-# for resp_num, resp in enumerate(upload_responses):
-#     logger.info(f"processing upload response: {resp_num}/{len(upload_responses)}")
-#     try:
-#         if resp.check_output_file_exists():
-#             df_uploads_ = pd.DataFrame(resp.read_output_data())
-#             df_uploads = pd.concat([df_uploads, df_uploads_])
-#         else:
-#             missing_files = missing_files + [resp.get_output_file()]
-#     except Exception as e:
-#         logger.info(f"Error in processing upload response: {e}")
+"""
+Time taken to upload 55 documents: 8.278589681784313 min
+"""
 # ### define async tasks to read output data
+start_time = time.time()
 tasks = [
     resp.async_read_output_data()
     for resp_num, resp in enumerate(upload_responses)
@@ -79,6 +71,24 @@ df_uploads = [pd.DataFrame(df) for df in df_uploads]
 df_uploads = pd.concat(df_uploads)
 # reset index
 df_uploads = df_uploads.reset_index(drop=True)
+end_time = time.time()
+logger.info(
+    f"Time taken to read {len(df_uploads)} upload responses: "
+    f"{(end_time - start_time) / 60} min"
+)
+"""
+list(df_uploads.columns)
+['doc_name', 'file_type', 'filename', 'href', 'username']
+df_uploads.head().to_dict('records')
+[
+    {'doc_name': 'userid_stuartcullinan_uploadfilename_jason_08_gpgpdf', 'file_type': '.pdf', 'filename': 'jason_08_gpgpdf', 'href': 'gs://db-genie/entity_type=url/entity=userid_stuartcullinan_uploadfilename_jason_08_gpgpdf/data_type=unstructured/format=pdf/variable_desc=uploaded-document/source=stuartcullinan/jason_08_gpgpdf.pdf', 'username': 'stuartcullinan'}, 
+    {'doc_name': 'userid_stuartcullinan_uploadfilename_jeon_20_billerudkorsnas_annual-report_2021pdf', 'file_type': '.pdf', 'filename': 'jeon_20_billerudkorsnas_annual-report_2021pdf', 'href': 'gs://db-genie/entity_type=url/entity=userid_stuartcullinan_uploadfilename_jeon_20_billerudkorsnas_annual-report_2021pdf/data_type=unstructured/format=pdf/variable_desc=uploaded-document/source=stuartcullinan/jeon_20_billerudkorsnas_annual-report_2021pdf.pdf', 'username': 'stuartcullinan'}, 
+    {'doc_name': 'userid_stuartcullinan_uploadfilename_karishma-13-2021-air-new-zealand-gender-pay-reportpdf', 'file_type': '.pdf', 'filename': 'karishma-13-2021-air-new-zealand-gender-pay-reportpdf', 'href': 'gs://db-genie/entity_type=url/entity=userid_stuartcullinan_uploadfilename_karishma-13-2021-air-new-zealand-gender-pay-reportpdf/data_type=unstructured/format=pdf/variable_desc=uploaded-document/source=stuartcullinan/karishma-13-2021-air-new-zealand-gender-pay-reportpdf.pdf', 'username': 'stuartcullinan'}, 
+    {'doc_name': 'userid_stuartcullinan_uploadfilename_jeon_25_upm_annual-report_2021pdf', 'file_type': '.pdf', 'filename': 'jeon_25_upm_annual-report_2021pdf', 'href': 'gs://db-genie/entity_type=url/entity=userid_stuartcullinan_uploadfilename_jeon_25_upm_annual-report_2021pdf/data_type=unstructured/format=pdf/variable_desc=uploaded-document/source=stuartcullinan/jeon_25_upm_annual-report_2021pdf.pdf', 'username': 'stuartcullinan'}, 
+    {'doc_name': 'userid_stuartcullinan_uploadfilename_karishma-13-anti-bribery-and-corruption-policy-august-2021pdf', 'file_type': '.pdf', 'filename': 'karishma-13-anti-bribery-and-corruption-policy-august-2021pdf', 'href': 'gs://db-genie/entity_type=url/entity=userid_stuartcullinan_uploadfilename_karishma-13-anti-bribery-and-corruption-policy-august-2021pdf/data_type=unstructured/format=pdf/variable_desc=uploaded-document/source=stuartcullinan/karishma-13-anti-bribery-and-corruption-policy-august-2021pdf.pdf', 'username': 'stuartcullinan'}
+]
+"""
+
 
 # ### get uploaded document names
 doc_names = df_uploads['doc_name'].unique().tolist()
