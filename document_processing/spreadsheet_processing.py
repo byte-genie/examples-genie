0c00d6b84ca154caff7c9f4f8ad9b810195087d0
mh@imac <mjdhasan@icloud.com>
2023-10-19 11:57:51 +0800

    added document_processing/spreadsheet_processing.py

---
+++ document_processing/spreadsheet_processing.py
@@ -0,0 +1,8 @@
+# <h1>Spreadsheet processing</h1> <br />
+# <p>This example covers
+#     <ul>
+#         <li>Extracting data from spreadsheets;</li>
+#         <li>Re-structuring data in a desired format;</li>
+#         <li>Saving re-structured data into a relational database.</li>
+#     </ul>
+# </p> <br />
